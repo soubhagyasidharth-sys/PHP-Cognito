5b3e4f9b09d7043ad7ed118121916007b64f68b9
soubhagyasidharth-sys <soubhagyasidharth@gmail.com>
2026-03-12 14:28:28 +0530

    Implement OAuth2 callback route
    Handle OAuth2 token exchange using authorization code.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/callback', function () {
-    return request()->all();
+
+    $code = request('code');
+
+    $response = Http::asForm()->post(
+        'https://ap-south-1hq2ed9iqe.auth.ap-south-1.amazoncognito.com/oauth2/token',
+        [
+            'grant_type' => 'authorization_code',
+            'client_id' => 'r0pngth0fva0b7m5fjplei6b1',
+            'code' => $code,
+            'redirect_uri' => 'http://3.108.160.157/callback'
+        ]
+    );
+
+    return $response->json();
 });
