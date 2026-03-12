e959226c4bbfe0176b60afdda3f3ee1bf269aa98
soubhagyasidharth-sys <soubhagyasidharth@gmail.com>
2026-03-12 16:01:09 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,17 @@ Route::get('/callback', function () {
 
     $code = request('code');
 
-    $response = Http::asForm()->post(
-        'https://ap-south-1hq2ed9iqe.auth.ap-south-1.amazoncognito.com/oauth2/token',
-        [
-            'grant_type' => 'authorization_code',
-            'client_id' => 'r0pngth0fva0b7m5fjplei6b1',
-            'code' => $code,
-            'redirect_uri' => 'http://3.108.160.157/callback'
-        ]
-    );
+    $response = Http::asForm()
+        ->withBasicAuth('CLIENT_ID', 'CLIENT_SECRET')
+        ->post(
+            'https://ap-south-1hq2ed9iqe.auth.ap-south-1.amazoncognito.com/oauth2/token',
+            [
+                'grant_type' => 'authorization_code',
+                'client_id' => 'CLIENT_ID',
+                'code' => $code,
+                'redirect_uri' => 'http://3.108.160.157/callback'
+            ]
+        );
 
     return $response->json();
 });
