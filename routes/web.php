211327a5e99cf7cb7197f0cf1deda633c6fdaaba
soubhagyasidharth-sys <soubhagyasidharth@gmail.com>
2026-03-12 16:02:36 +0530

    Change OAuth2 basic auth credentials
    Updated basic authentication credentials for OAuth2 token request.

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/callback', function () {
     $code = request('code');
 
     $response = Http::asForm()
-        ->withBasicAuth('CLIENT_ID', 'CLIENT_SECRET')
+        ->withBasicAuth('7l4pbndvbv46r3ghlgkbi4msrt', '19n28gq937p9lq3370srhadt53odektom2rqjv9u70q3fg4n6ln')
         ->post(
             'https://ap-south-1hq2ed9iqe.auth.ap-south-1.amazoncognito.com/oauth2/token',
             [
