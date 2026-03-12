2afd01621d01cebe9606170e65b3d393c32c6e48
soubhagyasidharth-sys <soubhagyasidharth@gmail.com>
2026-03-12 16:50:44 +0530

    Update OAuth token request with new credentials

--- routes/web.php
+++ routes/web.php
@@ -12,14 +12,17 @@ Route::get('/callback', function () {
     $code = request('code');
 
     $response = Http::asForm()
-        ->withBasicAuth('7l4pbndvbv46r3ghlgkbi4msrt', '19n28gq937p9lq3370srhadt53odektom2rqjv9u70q3fg4n6ln')
+        ->withBasicAuth(
+            '7l4pbndvbv46r3ghlgkbi4msrt',
+            '19n28gq937p9lq3370srhadt53odektom2rqjv9u70q3fg4n6ln'
+        )
         ->post(
             'https://ap-south-1hq2ed9iqe.auth.ap-south-1.amazoncognito.com/oauth2/token',
             [
                 'grant_type' => 'authorization_code',
-                'client_id' => 'CLIENT_ID',
+                'client_id' => '7l4pbndvbv46r3ghlgkbi4msrt',
                 'code' => $code,
-                'redirect_uri' => 'http://3.108.160.157/callback'
+                'redirect_uri' => 'https://cognito-redirect.vercel.app'
             ]
         );
 
